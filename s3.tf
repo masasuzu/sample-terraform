accb8f1359a3ee519adb9d7b7157c6712b809167
SUZUKI, Masashi <ma.suzuki@bengo4.com>
2021-08-30 11:40:54 +0900

    add delete rule

--- s3.tf
+++ s3.tf
@@ -2,9 +2,27 @@ resource "aws_s3_bucket" "main" {
   bucket = "sample-s3-bucket-sugoiyatsu"
   acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+    id      = "delete_rule"
+
+    expiration {
+      days = 3
+    }
+
+    noncurrent_version_expiration {
+      days = 3
+    }
+  }
+
   tags = {
     Name        = "sample-s3-bucket-sugoiyatsu"
     Environment = "Dev"
     SomeTag     = "hogehoge"
+    SugoiTag    = "ChoSugoi"
   }
 }
